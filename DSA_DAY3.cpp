0576dc2744106b07893e7929c9eda4f32f8b9daa
Ankit Pathak <pathakankit688@gmail.com>
2024-08-22 06:08:06 +0530

    DSA DAY 3 in cpp

---
+++ DSA_DAY3.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//  // cout << "Sunfire Sensei"<<  endl;
+//    // cout << "Sunfire Sensei ";
+//    cout << 3*9;
+// }
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // int Number; // declare a variable of type int
+  // Number = 100; // assign a value to the variable
+
+  // cout <<  Number << endl;
+
+  // float f=1.2;
+  // cout << f << endl;
+
+  // double d= 1.654554655645544;
+  // cout << d << endl;
+
+  // string s = "Sunfire Sensei";
+  // cout << s << endl;
+  // cout << s << endl;
+  // cout << s << endl;
+  // cout << s << endl;
+
+  // bool b= false;
+  // cout << b << endl;
+
+  char32_t a = 'A';
+  cout << a << endl;
+}
