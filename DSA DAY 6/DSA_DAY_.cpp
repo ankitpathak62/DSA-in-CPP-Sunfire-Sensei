6e9d3ba2ac4cb5c05247adf5953daefde698aa46
Ankit Pathak <pathakankit688@gmail.com>
2024-08-22 08:10:51 +0530

    dsa day 6

---
+++ DSA DAY 6/DSA_DAY_.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // int row, col;
+
+  // for(row=1; row<=4; row=row+1){
+  //   //print * 4 times
+  //   for(col=1; col<=4; col=col+1){
+  //     cout<<"*"<<" ";
+
+  //   }
+  //   cout<<endl;
+  // }
+
+  // int row, col;
+
+  // for(row=1; row<=4; row=row+1){
+  //   //print 4, 4 times
+  //   for(col=1; col<=4; col=col+1){
+  //     cout<<4<<" ";
+
+  //   }
+  //   cout<<endl;
+  // }
+
+  // int row,col;
+
+  //   for(row=1; row<=8; row=row+1){
+  //     for(col=1; col<=4; col=col+1){
+  //        cout<<row<<" ";
+  //     }
+  //     cout<<endl;
+  //   }
+
+  // int row, col;
+
+  //   for(row=1; row<=4;row=row+1){
+  //     for(col=1; col<=4; col=col+1){
+  //       cout<< col<<" ";
+  //     }
+  //     cout<<endl;
+  //   }
+
+  // int row, col;
+  // for (row = 1; row <= 4; row = row + 1) {
+    // for (col = 4; col >= 1; col = col - 1) {
+      // cout << col << " ";
+    // }
+    // cout << endl;
+  // }
+
+  
+
+
+
+
+  // int row, col;
+  //  for(row=1; row<=5;row=row+1){
+  //    for(col=1; col<=5; col=col+1){
+  //      cout<< col*col*col<<" ";
+  //    }
+  //    cout<<endl;
+  //  }
+
+// int row, col;
+//   for(row=1; row<=4;row=row+1){
+//     char SS = 'a'+(row-1);
+//     for(col=1; col<=4; col=col+1){
+//       cout<< SS<<" ";
+//     } 
+//     cout<<endl;
+    
+//   }
+
+
+
+
+
+
+
+  // int row;
+  // char col;
+  // for(row=1; row<=4;row=row+1){
+  //   for( col='a'; col<='d' ; col=col+1){
+  //     cout<< col<<" ";
+  //   }
+  //   cout<<endl;
+  // }
+
+
+
+  // int row,col;
+  // for(row=1; row<=4;row=row+1){
+  //   for(col=1; col<=4; col=col+1){
+  //   char SS = 'a'+(col-1);
+  //     cout<< SS <<" ";
+  //   }
+  //   cout<<endl;
+  // }
+  
+ 
+  // int row,col, count = 1;
+  // for(row=1; row<=4;row=row+1){
+  //   for(col=1; col<=4; col=col+1){
+  //     cout<< count <<" ";
+  //     count = count + 1;
+  //   }
+  //   cout<<endl;
+  // }
+    
+
+
+
+
+
+  int row,col ;
+  for(row=1; row<=4;row=row+1){
+    for(col=1; col<=4; col=col+1){
+       cout<<(row-1)*4+col<<"      ";
+    }
+    cout<<endl;
+  }
+
+
+  
+  
+  
+}
