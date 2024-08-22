140bac658938e41be8b43fcace29b9e4a8e4858a
Ankit Pathak <pathakankit688@gmail.com>
2024-08-22 06:10:57 +0530

    DSA DAY 4 in c++

---
+++ DSA_DAY4.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // int a,b;
+  // cout<<"Enter the value of a: ";
+  // cin>>a;
+  // cout<<"Enter the value of b: ";
+  // cin>>b;
+  // cout<<"Sum of a and b is "<<a+b;
+
+  // int a= 66;
+  // char b= 'A';
+
+  // cout<<a+b;
+
+  // int money ;
+  // cout<<"Enter the amount of money you have: ";
+  // cin>>money;
+
+  // if(money >= 100){
+  //   cout<<" I'm able to Buy";
+  // }
+  // else{
+  //   cout<<" I'm not able to Buy";
+  // }
+
+  // int marks;
+  // cout<<"Enter the marks: ";
+  // cin>>marks;
+  // if(marks >= 35){
+  //   cout<<"You are pass";
+  // }
+  // else{
+  //   cout<<"You are fail";
+  // }
+
+  // int num;
+  // cout << "Enter the number: ";
+  // cin >> num;
+
+  // if (num > 0) {
+  //   cout << "Number is positive";
+  // } 
+  // else {
+  //   if (num == 0) {
+  //     cout << "Number is zero";
+  //   } else {
+  //     cout << "Number is negative";
+  //   }
+  // }
+
+
+
+  // int daynum;
+  // cout<<"Enter the day number: ";
+  // cin>>daynum;
+
+  // if(daynum == 1){
+  //   cout<<"Monday";
+  // }
+  // else if(daynum == 2){
+  //   cout<<"Tuesday";
+  // }
+  // else if(daynum == 3){
+  //   cout<<"Wednesday";
+  // }
+  // else if (daynum == 4){
+  //   cout<<"Thursday";
+  // }
+  // else if(daynum == 5){
+  //   cout<<"Friday";
+  // }
+  // else if(daynum == 6){
+  //   cout<<"Saturday";
+  // }
+  // else if(daynum == 7){
+  //   cout<<"Sunday";
+  // }
+  // else{
+  //   cout<<"Invalid day number";
+  // }
+
+
+  for(int count =1; count<=10; count = count +1 ){
+    cout<<"Sunfire Sensei\n";
+  }
+
+  
+//print n natural number
+  // int num ;
+  // cout<<"Enter the number: ";
+  // cin>>num;
+  // for(int i = 1 ; i<=num; i = i+1){
+  //   cout<<i*i<<endl;
+  // }
+
+// int num;
+//   cout<<"Enter the number: ";
+//   cin>>num;
+//   for(int i = 2; i<=num; i = i+2){
+//     cout<<i<<endl;
+//   }
+
+// int num;
+//   cout<<"Enter the number: ";
+//   cin>>num;
+//   for(int i = 1; i<=num; i = i+2){
+//     cout<<i<<endl;
+//   }
+
+
+  // int num ;
+  // cout<<"Enter the number: ";
+  // cin>>num;
+  // for(int i = 1; i<=num; i = i+1){
+  //   if(i%2 == 0){
+  //     cout<<i<<endl;
+  //   }
+  // }
+   
+
+  
+
+  
+}
