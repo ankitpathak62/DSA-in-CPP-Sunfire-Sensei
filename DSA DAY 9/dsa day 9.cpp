26c00c2f368dbd30464f3c8e254a6cb62501e830
Ankit Pathak <pathakankit688@gmail.com>
2024-08-27 21:19:55 +0530

    DSA DAY 9 Code

---
+++ DSA DAY 9/dsa day 9.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int n, row, col;
+  cout << " Enter a number ";
+  cin >> n;
+
+  //   for (row = 1; row <= n; row++) {
+  //     // print space
+  //     for (col = 1; col <= n - row; col++) {
+  //       cout << " ";
+  //     }
+  //     // print stars
+  //     for (col = 1; col <= 2 * row - 1; col++) {
+  //       cout << "*";
+  //     }
+  //     cout << endl;
+  //   }
+
+  // for( row=n; row>=1; row--){
+
+  //   // print space
+
+  //   for( col=1; col<=n-row; col++){
+  //     cout<<" ";
+  //   }
+  //   //print *
+  //   for( col=1; col<=2*row-1; col++){
+  //     cout<<"*";
+  //   }
+  //   cout<< endl;
+  // }
+
+  // for (row = 1; row <= n; row++) {
+  //   // print space
+
+  //   for (col = 1; col <= n - row; col++) {
+  //     cout << " ";
+  //   }
+  //   // print 1 to row
+  //   for (col = 1; col <= row; col++) {
+  //     cout << col;
+  //   }
+  //   // print row-1 to 1
+  //   for (col = row - 1; col >= 1; col--) {
+  //     cout << col;
+  //   }
+  //   cout << endl;
+  // }
+
+
+
+  // for(row=n ; row>= 1; row--){
+  //   // print * row times
+  //   for(col=1; col<=row; col++){
+  //     cout<<"*";
+  //   }
+  //   // print space 2n-2row times
+  //   for(col=1; col<=2*n-2*row; col++){
+  //     cout<<" ";
+  //   }
+  //   // print * row times
+  //   for(col=1; col<=row; col++){
+  //     cout<<"*";
+  //   }
+  //   cout<< endl;
+  // }
+
+  // //lower part
+  // for(row=1 ; row<= n; row++){
+  //   // print * row times
+  //   for(col=1; col<=row; col++){
+  //     cout<<"*";
+  //   }
+  //   // print space 2n-2row times
+  //   for(col=1; col<=2*n-2*row; col++){
+  //     cout<<" ";
+  //   }
+  //   // print * row times
+  //   for(col=1; col<=row; col++){
+  //     cout<<"*";
+  //   }
+  //   cout<< endl;
+  // }
+
+
+
+
+
+
+// for( row=1; row<=n; row++){
+//   // print * row times
+//   for( col=1; col<=row; col++){
+//     cout<<"*";
+//   }
+//   //print space 2n-2row times
+//   for( col=1; col<=2*n-2*row; col++){
+//     cout<<" ";
+//   }
+//   // print * row times
+//   for( col=1; col<=row; col++){
+//     cout<<"*";
+//   }
+//   cout<< endl;
+// }
+//   //lower part
+// for( row=n-1; row>=1; row--){
+//   // print * row times
+//   for( col=1; col<=row; col++){
+//     cout<<"*";
+//   }
+//   //print space 2n-2row times
+//   for( col=1; col<=2*n-2*row; col++){
+//     cout<<" ";
+//   }
+//   // print * row times
+//   for( col=1; col<=row; col++){
+//     cout<<"*";
+//   }
+//   cout<< endl;
+// }
+
+ 
+  
+}
